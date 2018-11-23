8abdd9abe7485d4cec0045725624c9ce86b73451
wangsubin <574737015@qq.com>
2018-11-23 15:02:11 +0800

    查询客户信息消费者

--- microservice-simple-consumer-movie/src/main/java/com/subin/cloud/study/user/controller/MovieController.java
+++ microservice-simple-consumer-movie/src/main/java/com/subin/cloud/study/user/controller/MovieController.java
@@ -2,6 +2,7 @@ package com.subin.cloud.study.user.controller;
 
 import com.subin.cloud.study.user.entity.User;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
@@ -21,6 +22,10 @@ public class MovieController {
 
     @GetMapping("/user/{id}")
     public User findById(@PathVariable Long id){
+        //封装了返回的响应信息,包括 响应状态,响应头 和 响应体.
+        //ResponseEntity<User> entity = restTemplate.getForEntity("http://localhost:8000/" + id, User.class);
+
+        //响应体
         return restTemplate.getForObject("http://localhost:8000/"+id,User.class);
     }
 
